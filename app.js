d80f2aa497960efc6c1adcdc4e45cfa5e7b8b667
javidalakbarli <73642700+javidalakbarli@users.noreply.github.com>
2022-11-29 13:13:11 +0400

    added new features and css design

--- app.js
+++ app.js
@@ -1,52 +1,95 @@
-$(window).on('load', function () {
+$("#cityForm").on('submit', (e) => {
+    let city = $('#city').val();
+
+    findWeatherByCity(city);
+    e.preventDefault();
+});
+
+$("#coordForm").on('submit', (e) => {
+    let latitude = $('#lat').val();
+    let longitude = $('#lon').val();
+
+    getByCoord(latitude, longitude);
+    e.preventDefault();
+});
+
+$("#geoForm").on('submit', (e) => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
+    e.preventDefault();
 });
+
+function getByCoord(latitude, longitude) {
+    findWeatherByCoord(latitude, longitude);
+}
 
 function showPosition(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
+
     console.log("Latitude: " + latitude);
     console.log("Longitude: " + longitude);
 
-    findWeather(latitude, longitude);
+    findWeatherByCoord(latitude, longitude);
 }
 
-function findWeather(latitude, longitude) {
+function findWeatherByCity(city) {
+    $(".inner-main-containers").remove();
+    $(".inner-detail-containers").remove();
+    $(".inner-footer-containers").remove();
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9573f03a457a72999cd19e9b55cc7767&units=metric`)
+        .then((response) => {
+            createData(response);
+        })
+}
+
+function findWeatherByCoord(latitude, longitude) {
+    $(".inner-main-containers").remove();
+    $(".inner-detail-containers").remove();
+    $(".inner-footer-containers").remove();
+
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=9573f03a457a72999cd19e9b55cc7767&units=metric`)
         .then((response) => {
-            console.log(response);
+            createData(response);
+        })
+}
 
-            let city = response.data.name;
-            let weatherCondition = response.data.weather[0].main;
-            let weatherConditionDescription = response.data.weather[0].description;
+function createData(response) {
+    console.log(response);
 
-            let temp = response.data.main.temp;
-            let tempMax = response.data.main.temp_max;
-            let tempMin = response.data.main.temp_min;
+    let city = response.data.name;
+    let countryCode = response.data.sys.country;
 
-            let feelsLike = response.data.main.feels_like;
-            let humidity = response.data.main.humidity;
-            let pressure = response.data.main.pressure;
+    let weatherCondition = response.data.weather[0].main;
+    let weatherConditionDescription = response.data.weather[0].description;
 
-            let windSpeed = response.data.wind.speed;
-            let windDeg = response.data.wind.deg;
-            let windDirection = getDirection(windDeg);
+    let temp = response.data.main.temp;
+    let tempMax = response.data.main.temp_max;
+    let tempMin = response.data.main.temp_min;
 
-            let unix = response.data.dt;
-            // Create a new JavaScript Date object based on the timestamp
-            // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-            let date = new Date(unix * 1000);
+    let feelsLike = response.data.main.feels_like;
+    let humidity = response.data.main.humidity;
+    let pressure = response.data.main.pressure;
 
-            let localTime = date.toLocaleTimeString("en-US")
-            let localDate = date.toLocaleDateString("en-US");
+    let windSpeed = response.data.wind.speed;
+    let windDeg = response.data.wind.deg;
+    let windDirection = getDirection(windDeg);
 
-            getFullCountryName(city.split(" ")[0]);
+    let unix = response.data.dt;
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    let date = new Date(unix * 1000);
 
-            $('.weather-details').append(`  <div class="inner-main-containers">
+    let localTime = date.toLocaleTimeString("en-US")
+    let localDate = date.toLocaleDateString("en-US");
+
+    getFullCountryName(countryCode, city.split(' ')[0]);
+
+    $('.weather-details').append(`  <div class="inner-main-containers">
                                                 <div id="importantContainer">
                                                     <h2>${Math.ceil(temp)}<sup>°</sup>C</h2>
                                                     <hr />
@@ -80,14 +123,15 @@ function findWeather(latitude, longitude) {
                                                 <p>${localTime}</p>
                                                 <p>${localDate}</p>
                                             </div>`);
-        })
 }
 
-function getFullCountryName(city) {
-    axios.get(`https://restcountries.com/v3.1/capital/${city}`)
+function getFullCountryName(countryCode, city) {
+    $(".inner-head-container").remove();
+    axios.get(`https://restcountries.com/v3.1/alpha/${countryCode}`)
         .then((response) => {
-            let country = response.data[0].altSpellings[1];
-            $('.weather-head').append(`<h1><i>${country}, ${city}</i></h1>`);
+            console.log(response);
+            let country = response.data[0].name.common;
+            $('.weather-head').append(`<div class="inner-head-container"><h1><i>${country}, ${city}</i></h1></div>`);
         })
 }
 
