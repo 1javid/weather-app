a43587332687f83abbbf3a120e51c66191563570
javidalakbarli <73642700+javidalakbarli@users.noreply.github.com>
2022-12-01 01:05:49 +0400

    fixed semantical error

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ function getFullCountryName(countryCode, city) {
 }
 
 function getDirection(angle) {
-    var directions = ['North', 'North-Easy', 'East', 'South-East', 'South', 'South-West', 'West', 'North-West'];
+    var directions = ['North', 'North-East', 'East', 'South-East', 'South', 'South-West', 'West', 'North-West'];
     var index = Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 45) % 8;
     return directions[index];
 }
